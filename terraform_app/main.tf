065cfc3a260f5f6d2ee40979f8e7c5d3a9b4ba06
Divyanshi-Pathak <2002divyanship@gmail.com>
2025-04-25 22:57:08 +0530

    Update main.tf

--- terraform_app/main.tf
+++ terraform_app/main.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.26.0"
+      version = "~> 3.107.0"
     }
   }
-  required_version = ">= 1.1.0"
 }
 provider "azurerm" {
   features {}
